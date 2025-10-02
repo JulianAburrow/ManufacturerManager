6b6eab8f1691b658e58ed80bb3488b2d10cd2434
Julian Aburrow <jaburrow@btinternet.com>
2025-10-02 13:15:33 +0100

    Increased global timeout

--- TestsPlaywright/GlobalValues.cs
+++ TestsPlaywright/GlobalValues.cs
@@ -4,7 +4,7 @@ public static class GlobalValues
 {
     public static string BaseUrl => Environment.GetEnvironmentVariable("BASE_URL") ?? "https://127.0.0.1:5245";
     public const bool IsHeadless = true;
-    public const int GlobalTimeOut = 120_000;
+    public const int GlobalTimeOut = 150_000;
     public static PageGotoOptions GetPageOptions() =>
         new()
         {
