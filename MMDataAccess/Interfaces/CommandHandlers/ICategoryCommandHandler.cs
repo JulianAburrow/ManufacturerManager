a83676424506605c641eab5b8e501792a5872704
Julian Aburrow <vdub1992@hotmail.co.uk>
2026-01-16 15:40:07 +0000

    Added CategoryCommandHandler and appropriate tests

---
+++ MMDataAccess/Interfaces/CommandHandlers/ICategoryCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace MMDataAccess.Interfaces.CommandHandlers;
+
+public interface ICategoryCommandHandler
+{
+    Task CreateCategoryAsync(CategoryModel category, bool callSaveChanges);
+
+    Task UpdateCategoryAsync(CategoryModel category, bool callSaveChanges);
+
+    Task DeleteCategoryAsync(int categoryId, bool callSaveChanges);
+
+    Task SaveChangesAsync();
+}
