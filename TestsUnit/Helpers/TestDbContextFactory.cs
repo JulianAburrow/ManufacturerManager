4ae963f66349b28551606f0651c97e00554f34cc
JulianAburrow <75375767+JulianAburrow@users.noreply.github.com>
2026-07-10 14:19:08 +0100

    Bug/change test incorrect testunit helper (#76)
    * Change unit test helper to return correct type
    
    ---------
    
    Co-authored-by: Julian Aburrow <vdub1992@hotmail.co.uk>

---
+++ TestsUnit/Helpers/TestDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace TestsUnit.Helpers;
+
+public class TestDbContextFactory : IDbContextFactory<ManufacturerManagerContext>
+{
+    private readonly DbContextOptions<ManufacturerManagerContext> _options;
+
+    public TestDbContextFactory(DbContextOptions<ManufacturerManagerContext> options)
+    {
+        _options = options;
+    }
+
+    public ManufacturerManagerContext CreateDbContext()
+    {
+        return new ManufacturerManagerContext(_options);
+    }
+}
